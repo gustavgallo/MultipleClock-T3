4a1781563b22687faebab17765ae9b04c9d444c3
Gustavo Gallo <gustavotibollagallo@gmail.com>
2025-05-26 20:03:22 -0300

    ajeitei os estados do deserializer pra que tivesse só 2 estados sendo assim mais facil de controlar, parece estar pronto

--- source/deserializer.sv
+++ source/deserializer.sv
@@ -12,14 +12,10 @@ output logic data_ready // sinal pra enviar
 
 );
 
-
+//vamo simplificar as coisas, ou ta recebendo ou ta esperando e ja era
 typedef enum logic [1:0] { 
     
     RECEIVE,
-
-    WRITE,
-
-    SEND,
 
     WAIT
 
@@ -28,6 +24,8 @@ typedef enum logic [1:0] {
 state_t EA;
 
 
+logic [2:0] counter; // contador para saber quantos bits foram recebidos
+
 // always de dados
 always_ff @(posedge clock_100KHZ, posedge reset)begin
 
@@ -35,6 +33,8 @@ always_ff @(posedge clock_100KHZ, posedge reset)begin
         status_out <= 1; // está em condição de receber bits
         data_ready <= 0; // não ta pronto
         data_out <= 0; //zera a saída
+        counter <= 0; // reseta o contador
+
 
     end
     else begin
@@ -42,29 +42,28 @@ always_ff @(posedge clock_100KHZ, posedge reset)begin
         case(EA)
 
             RECEIVE:begin
-
-
-            end
-
-            WRITE:begin
-
-
-
-            end
-
-            SEND:begin
-
-
+                if (status_out) begin
+                    if (write_in) begin // se recebeu o write_in, significa que é pra receber o dado
+                        data_out <= {data_out[6:0], data_in}; // shift left e coloca o bit recebido no final
+                        if (counter == 3'd7) begin // se recebeu todos os bits escreve no data_ready
+                            data_ready <= 1;
+                            status_out <= 0; // nao pode receber mais dados
+                            
+                        end
+                    end
+                end
 
             end
-
+ 
             WAIT:begin
-
+                if (ack_in) begin // se recebeu o ack_in, significa que a fila ja tratou dos dados
+                    status_out <= 1; // pode receber mais dados
+                    data_ready <= 0; // nao esta pronto
+                    counter <= 0; // reseta o contador
+                    data_out <= 0; // reseta a saída
+                end
 
             end
-
-
-
 
         endcase
 
@@ -86,26 +85,15 @@ always_ff @(posedge clock_100KHZ, posedge reset)begin
         case(EA)
 
             RECEIVE:begin
-            if (write_in) EA <= WRITE;
-            else EA <= RECEIVE;
-
-            end
-
-            WRITE:begin
-           
-
-            end
-
-            SEND:begin
-
-
-
+            if (status_out) EA <= RECEIVE;;
+            else EA <= WAIT;
 
             end
 
             WAIT:begin
 
-
+            if (ack_in) EA <= RECEIVE; // se recebeu o ack_in, volta para receber dados
+            else EA <= WAIT; // se n recebeu o ack_in, continua esperando
 
 
             end
