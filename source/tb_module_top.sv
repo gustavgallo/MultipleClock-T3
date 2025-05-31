bfb3dace6b69015d74e92e91b5241a1656cc4904
Rodrigo Machado Gonçaves <rmgoncalves06@gmail.com>
2025-05-31 13:14:35 +0000

    refiz o tb, vamo ver se da certo

--- source/tb_module_top.sv
+++ source/tb_module_top.sv
@@ -2,49 +2,59 @@
 
 module tb_module_top;
 
-//geral
-logic reset = 1;
-logic clock = 0;
-//queue
-logic enqueue_in; //escreve
-logic dequeue_in; //escreve
-logic [3:0] len_out; 
-logic [7:0] data_out; 
-//deserializer
-logic data_in; // escreve
-logic data_ready; // escreve
-logic status_out; 
-logic write_in;
+   //geral
+   logic reset = 0;
+   logic clock = 0;
+   //queue
+   logic enqueue_in; //escreve
+   logic dequeue_in; //escreve
+   logic [3:0] len_out; 
+   logic [7:0] data_out; 
+   //deserializer
+   logic data_in; // escreve
+   logic data_ready; // escreve
+   logic status_out; 
+   logic write_in;
 
-// Instância do DUT (Device Under Test)
-top main(
+   // Instância do DUT (Device Under Test)
+   top main(
 
-.reset(reset),
-.clock(clock),
-.enqueue_in(enqueue_in),
-.dequeue_in(dequeue_in),
-.len_out(len_out),
-.data_out(data_out),
-.data_in(data_in),
-.data_ready(data_ready),
-.status_out(status_out),
-.write_in(write_in)
+      .reset(reset),
+      .clock(clock),
+      .enqueue_in(enqueue_in),
+      .dequeue_in(dequeue_in),
+      .len_out(len_out),
+      .data_out(data_out),
+      .data_in(data_in),
+      .data_ready(data_ready),
+      .status_out(status_out),
+      .write_in(write_in)
 
-);
-initial begin
+   );
+   initial begin
+      reset <= 1;
+      #100;
+      reset <= 0;
+   end
+   always begin
+       #500; clock <= ~clock; // clock de 1 MHz
+   end
+   initial begin 
+      #500;
+      data_in <= 0; #600;
+      data_in <= 1; #600;
+      data_in <= 0; #600;
+      data_in <= 1; #600;
+      data_in <= 0; #600;
+      data_in <= 0; #600;
+      data_in <= 1; #600;
+      data_in <= 0; #600; // o código "01010010" significa: R
 
-reset <= #50 ~reset; #50;  
+      enqueue_in <= 1; #600;
+      dequeue_in <= 1; #600;
 
-end
-always begin #500; clock <= ~clock; // clock de 1 MHz
-end
-always begin 
-   #500;
-   data_in <= 1; #600;
-   data_in <= 0; #600;
-end
+      #5000;
+      $finish;
+   end
 
-
-
-
-   endmodule
+ endmodule
